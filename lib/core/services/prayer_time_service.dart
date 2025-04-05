26738b55ff3f7dcbc504edd2eac793c64d970a66
Sobhy <sobhy.abdelhakam25@gmail.com>
2025-04-05 12:27:20 +0200

    add prayer time service class

---
+++ lib/core/services/prayer_time_service.dart
@@ -0,0 +1,23 @@
+import 'package:adhan_dart/adhan_dart.dart';
+import 'package:azan_app/core/services/location_service.dart';
+import 'package:geolocator/geolocator.dart';
+
+class PrayerTimeService {
+  // this class is responsible for calculating prayer times based on the user's location
+  
+  Future<Map<String, DateTime>> getPrayerTimes() async {
+    Position position = await LocationService().getUserLocation();
+    Coordinates coordinates = Coordinates(position.latitude, position.longitude);
+    CalculationParameters params = CalculationMethod.muslimWorldLeague();
+
+    PrayerTimes prayerTimes = PrayerTimes(coordinates: coordinates, calculationParameters: params, date: DateTime.now());
+
+    return {
+      'Fajr' : prayerTimes.fajr!,
+      'Dhuhr' : prayerTimes.dhuhr!,
+      'Asr' : prayerTimes.asr!,
+      'Maghrib' : prayerTimes.maghrib!,
+      'Isha' : prayerTimes.isha!,
+    };
+  }
+}
